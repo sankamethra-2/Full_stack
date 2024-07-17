7be58c88b194caf9f46399e3a9de0ce5f98e10b4
sankamethra <sankamethra2@gmail.com>
2024-07-17 18:02:31 +0530

    added-error-handling

--- node-rest/app.js
+++ node-rest/app.js
@@ -1,6 +1,7 @@
 const express =  require ('express');
 const app =  express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const productRoutes = require('./api/routes/products')
 const orderRoutes = require('./api/routes/orders')
@@ -9,6 +10,25 @@ const orderRoutes = require('./api/routes/orders')
 //     res.status(200).json({
 //         message: 'It works!'
 //     });
+// });
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// app.use((req, res, next) =>{
+//     const error = new Error('Not found');
+//     error.status= 404;
+//     next(error);
+// }) ;
+
+// app.use((error,req, res, next)=>{
+//     res.status(error.status || 500);
+//     res.json({
+//         error: {
+//             message: error.message
+//         }
+//     })
 // });
 
 app.use('/products',productRoutes);
